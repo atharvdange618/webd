ff1eed4a4a5fc7420b888fa4dc94aab31c5eb2a2
atharvdange618 <atharvdange618@gmail.com>
2023-08-29 19:57:29 +0530

    installed firebase

---
+++ Learning Firebase/dist/bundle.js
@@ -0,0 +1,32 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ (() => {
+
+eval("const firebaseConfig = {\r\n    apiKey: \"AIzaSyCe6Fe_CkUdE_0wffoqsqL3oYz-a9saSVg\",\r\n    authDomain: \"learning-firebase-88072.firebaseapp.com\",\r\n    projectId: \"learning-firebase-88072\",\r\n    storageBucket: \"learning-firebase-88072.appspot.com\",\r\n    messagingSenderId: \"333025109396\",\r\n    appId: \"1:333025109396:web:7e533eaafe4459e9dc1b81\"\r\n}\n\n//# sourceURL=webpack://webd/./src/index.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/index.js"]();
+/******/ 	
+/******/ })()
+;
---
+++ Learning Firebase/src/index.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCe6Fe_CkUdE_0wffoqsqL3oYz-a9saSVg",
+    authDomain: "learning-firebase-88072.firebaseapp.com",
+    projectId: "learning-firebase-88072",
+    storageBucket: "learning-firebase-88072.appspot.com",
+    messagingSenderId: "333025109396",
+    appId: "1:333025109396:web:7e533eaafe4459e9dc1b81"
+}
