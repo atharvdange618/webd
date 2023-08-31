28b94a519f4dfbb25c3c5990fa9e08edce1c29fd
atharvdange618 <atharvdange618@gmail.com>
2023-08-31 18:51:50 +0530

    portfolio website file upload, learning firebase

--- Learning Firebase/src/index.js
+++ Learning Firebase/src/index.js
@@ -1,3 +1,8 @@
+import { initializeApp } from 'firebase/app'
+import {
+    getFirestore, collection, getDocs, addDoc
+} from 'firebase/firestore'
+
 const firebaseConfig = {
     apiKey: "AIzaSyCe6Fe_CkUdE_0wffoqsqL3oYz-a9saSVg",
     authDomain: "learning-firebase-88072.firebaseapp.com",
@@ -6,3 +11,46 @@ const firebaseConfig = {
     messagingSenderId: "333025109396",
     appId: "1:333025109396:web:7e533eaafe4459e9dc1b81"
 }
+
+//init firebase app
+initializeApp(firebaseConfig)
+
+
+//init services
+const db = getFirestore()
+
+//collection ref
+const colRef = collection(db, 'books')
+
+//get collection data
+getDocs(colRef)
+    .then((snapshot) => {
+        let books = []
+        snapshot.docs.forEach((doc) => {
+            books.push({ ...doc.data(), id: doc.id })
+        })
+        console.log(books)
+    })
+    .catch(err => {
+        console.log(err.message)
+    })
+
+//adding documents
+const addBookForm = document.querySelector('.add')
+addBookForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    addDoc(colRef, {
+        title: addBookForm.title.value,
+        author: addBookForm.author.value,
+    })
+        .then(() => {
+            addBookForm.reset()
+        })
+})
+
+//deleting documents
+const deleteBookForm = document.querySelector('.delete')
+deleteBookForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+})
