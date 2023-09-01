ca9fce649c68af265337987342cc1230cebd752a
atharvdange618 <atharvdange618@gmail.com>
2023-09-02 02:09:23 +0530

    contact me section started

--- Beginner Projects/Portfolio website/assets/js/main.js
+++ Beginner Projects/Portfolio website/assets/js/main.js
@@ -113,9 +113,6 @@ let swiper = new Swiper('.portfolio__container', {
     }
 });
 
-/*==================== TESTIMONIAL ====================*/
-
-
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
 
 
