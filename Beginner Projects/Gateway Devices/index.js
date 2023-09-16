f2aa0bf5af731f6d0b5536271daec1f2e43f4895
atharvdange618 <atharvdange618@gmail.com>
2023-09-16 14:28:40 +0530

    login and home page done

---
+++ Beginner Projects/Gateway Devices/index.js
@@ -0,0 +1,16 @@
+function loginUser() {
+    // Get the values entered by the user
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+
+    var validUsername = "Admin";
+    var validPassword = "Admin@123";
+
+    if (username === validUsername && password === validPassword) {
+        window.location.href = "index.html";
+    } else {
+        alert("Invalid username or password. Please try again.");
+    }
+
+    return false;
+}
