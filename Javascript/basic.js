ad88c73b9cc6187451af4214d50032292f2abad0
atharvdange618 <atharvdange618@gmail.com>
2023-09-26 20:37:57 +0530

    variables, operators, precendence, data types

---
+++ Javascript/basic.js
@@ -0,0 +1,84 @@
+/*
+age
+23
+typeof age
+'number'
+a = 10
+10
+b = 34
+34
+a * b
+340
+a - b
+-24
+a + b
+44
+a / b
+0.29411764705882354
+10 * 9
+90
+2 ** 3
+8
+2 % 2
+0
+10 % 2
+0
+sum = a + b
+44
+diff = a - b
+-24
+multi = a * b
+340
+div = a / b
+0.29411764705882354
+modulo = a % b
+10
+a
+10
+b
+34
+modulo = b % a
+4
+power = a ** b
+1.0000000000000001e+34
+power = b ** a
+2064377754059776
+9 % 2
+1
+10 % 2 == 0
+true
+11 % 2 == 0
+false
+0 / 0
+NaN
+typeof NaN
+'number'
+(2+1) * 3
+9
+3 / 1 + 2 ** 2
+7
+4 + 1 * 6 /2 
+7
+age
+23
+age + 2
+25
+undefined
+let hindi = 80;
+let eng = 90;
+let math = 100;
+let avg = ( hindi + math + eng)/3;
+undefined
+age = age + 2
+25
+age
+25
+eng
+90
+math
+100
+let avg = ( hindi + math + eng) / 3;
+undefined
+avg
+90
+*/
