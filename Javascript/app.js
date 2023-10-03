036ca084a31d0a2e11e664181891292ac3e5fa42
atharvdange618 <atharvdange618@gmail.com>
2023-10-03 13:06:23 +0530

    template literals and operators learnt

---
+++ Javascript/app.js
@@ -0,0 +1,15 @@
+// console.log('Hello world')
+// console.log('Hello from atharv')
+// let a = 23
+// let b = 34
+// console.log('sum is :', a + b)
+
+let pencilPrice = 5;
+let eraserPrice = 10;
+// let output = "Total price is: " + (pencilPrice + eraserPrice) + " Rupees";
+let output = `Total price is: ${pencilPrice + eraserPrice}`;
+console.log(output)
+
+let a = 10;
+console.log(a++)
+console.log(++a)
