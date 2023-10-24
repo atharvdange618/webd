3201206413e3f98b83baa38dec896ab1d2259045
atharvdange618 <atharvdange618@gmail.com>
2023-10-25 01:37:31 +0530

    learning exports and imports

---
+++ Learning Node JS/script.js
@@ -0,0 +1,9 @@
+var daddy = require('./script2');
+
+var a = 1;
+
+var b = 2;
+
+var c = 3;
+
+console.log(daddy);
---
+++ Learning Node JS/script2.js
@@ -0,0 +1,4 @@
+var d = 12345;
+var f = 34;
+
+module.exports = { first: d, second: f };
