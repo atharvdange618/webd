072a683127d1e4cd6b02748792231420190162a3
atharvdange618 <atharvdange618@gmail.com>
2023-10-25 22:36:56 +0530

    info box section completed successfully

---
+++ Beginner Projects/Quiz Application with Timer/js/script.js
@@ -0,0 +1,40 @@
+//selecting all required elements
+const start_btn = document.querySelector(".start_btn button");
+const info_box = document.querySelector(".info_box");
+const exit_btn = info_box.querySelector(".buttons .quit");
+const continue_btn = info_box.querySelector(".buttons .restart");
+const quiz_box = document.querySelector(".quiz_box");
+const result_box = document.querySelector(".result_box");
+const option_list = document.querySelector(".option_list");
+const time_line = document.querySelector("header .time_line");
+const timeText = document.querySelector(".timer .time_left_txt");
+const timeCount = document.querySelector(".timer .timer_sec");
+
+// if startQuiz button clicked
+start_btn.onclick = () => {
+    info_box.classList.add("activeInfo"); //show info box
+}
+
+// if exit quiz button clicked
+exit_btn.onclick = () => {
+    info_box.classList.remove("activeInfo"); //hide info box
+}
+
+// if continue button clicked
+continue_btn.onclick = () => {
+    info_box.classList.remove("activeInfo"); //hide info box
+    quiz_box.classList.add("activeQuiz"); //show quiz box
+    showQuestions(0); //show first question by calling showQuestion function
+    queCounter(1); //passing 1 parameter to queCounter function
+    startTimer(15); //call startTimer function
+    startTimerLine(0); //call startTimer function
+}
+
+let timeValue = 15;
+let que_count = 0;
+let que_numb = 1;
+let userScore = 0;
+let counter;
+let counterLine;
+let widthValue = 0;
+
