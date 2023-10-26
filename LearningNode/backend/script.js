e4e2a1e8e1cc14746404e6f507663e366d839006
atharvdange618 <atharvdange618@gmail.com>
2023-10-27 00:49:15 +0530

    learnt serving static files through backend server

--- LearningNode/backend/script.js
+++ LearningNode/backend/script.js
@@ -1,16 +1,29 @@
 const express = require('express');
 const app = express();
 
-app.use("/", function (req, res, next) {
-    console.log("You have reached at the middleware and will be forwarded to the next handler");
-    next();
-})
+app.set("view engine", "ejs");
+
+// app.use("/", function (req, res, next) {
+//     console.log("You have reached at the middleware and will be forwarded to the next handler");
+//     next();
+// })
+
+app.use(express.static("./public"));
 
 app.get("/", function (req, res) {
-    res.send("The server is available now ");
+    res.render("index");
 })
 
-//to make routes dynamic we use : route
+app.get("/contact", function (req, res) {
+    res.render("contact", { name: "babdigang" });
+})
+
+app.get("/home", function (req, res) {
+    res.send("Welcome to the home page");
+})
+
+//to make routes dynamic we use : where we want the route to be dynamically loaded
+//and to access the route's parameters we use req.params
 app.get("/profile/:username", function (req, res) {
     res.send(`Hello from ${req.params.username}`);
 })
