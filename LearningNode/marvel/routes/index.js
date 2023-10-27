fd239c8851d325da0a20d8ad4d18422cd651f158
atharvdange618 <atharvdange618@gmail.com>
2023-10-28 01:06:41 +0530

    learnt sessions on browsers

--- LearningNode/marvel/routes/index.js
+++ LearningNode/marvel/routes/index.js
@@ -1,9 +1,52 @@
 var express = require('express');
 var router = express.Router();
+const userModel = require("./users");
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
+router.get('/', function (req, res) {
+  req.session.ban = true; // Change "res.session" to "req.session"
   res.render('index', { title: 'Express' });
+});
+
+router.get('/create', async function (req, res) {
+  const createdUser = await userModel.create({
+    username: "Atharvdange._",
+    name: "Atharv",
+    age: 21
+  });
+  res.send(createdUser);
+});
+
+router.get('/allusers', async function (req, res) {
+  let allUsers = await userModel.find();
+  res.send(allUsers);
+});
+
+router.get('/user', async function (req, res) {
+  let user = await userModel.findOne({
+    username: "Atharvdange._"
+  });
+  res.send(user);
+});
+
+router.get('/delete', async function (req, res) {
+  let deleteUser = await userModel.findOneAndDelete({
+    username: "Atharvdange._"
+  });
+  res.send(deleteUser);
+});
+
+router.get('/checkban', function (req, res) {
+  if (req.session.ban === true) { // Change "res.session" to "req.session"
+    res.send("You are banned");
+  }
+});
+
+router.get('/removeban', function (req, res) {
+  req.session.destroy(function(err){
+    console.log(err);
+    res.send("You are unbanned");
+  });
 });
 
 module.exports = router;
--- LearningNode/marvel/routes/users.js
+++ LearningNode/marvel/routes/users.js
@@ -7,8 +7,14 @@ router.get('/', function (req, res, next) {
   res.send('respond with a resource');
 });
 
-module.exports = router;
-
 mongoose.connect("mongodb://127.0.0.1:27017/nodepractice");
 
+const userSchema = mongoose.Schema({
+  username: String,
+  name: String,
+  age: Number
+});
 
+const User = mongoose.model("User", userSchema); // Define the model and store it in a variable
+
+module.exports = User; // Export the User model
