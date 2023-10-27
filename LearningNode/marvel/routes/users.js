b2e199044f7ba10e530901d478130d542f52bc14
atharvdange618 <atharvdange618@gmail.com>
2023-10-28 00:03:44 +0530

    added mongoose to the marvel folder

--- LearningNode/marvel/routes/users.js
+++ LearningNode/marvel/routes/users.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+const mongoose = require('mongoose');
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
@@ -7,3 +8,7 @@ router.get('/', function (req, res, next) {
 });
 
 module.exports = router;
+
+mongoose.connect("mongodb://127.0.0.1:27017/nodepractice");
+
+
