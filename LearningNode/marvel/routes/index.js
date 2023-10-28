6def8bccbc9f48552f11c57eab143ab0e7b9ce4f
atharvdange618 <atharvdange618@gmail.com>
2023-10-28 19:27:35 +0530

    added comments to routes for better understanding

--- LearningNode/marvel/routes/index.js
+++ LearningNode/marvel/routes/index.js
@@ -1,60 +1,90 @@
+// Import necessary modules
 var express = require('express');
 var router = express.Router();
+
+// Import the "userModel" from the "users.js" module
 const userModel = require("./users");
 
 /* GET home page. */
 router.get('/', function (req, res) {
+  // Set a session variable "ban" to true
   req.session.ban = true;
+
+  // Set a cookie named "age" with a value of 25
   res.cookie("age", 25);
+
+  // Render the "index" view and pass the title "Express" to it
   res.render('index', { title: 'Express' });
 });
 
+// Route to read a cookie
 router.get('/read', function (req, res) {
+  // Log the value of the "age" cookie
   console.log(req.cookies.age);
+
+  // Send a response to the client
   res.send("check your cookies");
 });
 
+// Route to delete the "age" cookie
 router.get('/deletecookies', function (req, res) {
+  // Clear the "age" cookie
   res.clearCookie("age");
+
+  // Send a response to the client
   res.send("cookies deleted");
 });
 
+// Route to create a new user in the database
 router.get('/create', async function (req, res) {
   const createdUser = await userModel.create({
     username: "Atharvdange._",
     name: "Atharv",
     age: 21
   });
+
+  // Send the created user as a response
   res.send(createdUser);
 });
 
+// Route to fetch all users from the database
 router.get('/allusers', async function (req, res) {
   let allUsers = await userModel.find();
+
+  // Send all users as a response
   res.send(allUsers);
 });
 
+// Route to fetch a specific user from the database
 router.get('/user', async function (req, res) {
   let user = await userModel.findOne({
     username: "Atharvdange._"
   });
+
+  // Send the user as a response
   res.send(user);
 });
 
+// Route to delete a specific user from the database
 router.get('/delete', async function (req, res) {
   let deleteUser = await userModel.findOneAndDelete({
     username: "Atharvdange._"
   });
+
+  // Send the deleted user as a response
   res.send(deleteUser);
 });
 
+// Route to check if the user is banned based on the session variable
 router.get('/checkban', function (req, res) {
-  if (req.session.ban === true) { // Change "res.session" to "req.session"
+  if (req.session.ban === true) {
     res.send("You are banned");
   } else {
     res.send("You are not banned");
   }
 });
 
+// Route to remove the ban by destroying the session
 router.get('/removeban', function (req, res) {
   req.session.destroy(function (err) {
     console.log(err);
@@ -62,4 +92,4 @@ router.get('/removeban', function (req, res) {
   });
 });
 
-module.exports = router;
+module.exports = router; // Export the router module for use in other parts of your application
--- LearningNode/marvel/routes/users.js
+++ LearningNode/marvel/routes/users.js
@@ -1,20 +1,32 @@
+// Import necessary modules
 var express = require('express');
 var router = express.Router();
 const mongoose = require('mongoose');
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
-  res.send('respond with a resource');
+  res.send('respond with a resource'); // This route just sends a response when accessed
 });
 
+// Connect to MongoDB
 mongoose.connect("mongodb://127.0.0.1:27017/nodepractice");
+// The above line establishes a connection to a MongoDB server running on the local machine at port 27017.
+// It assumes a database named "nodepractice" is present; if not, MongoDB will create it.
 
+// Create a schema for the user
 const userSchema = mongoose.Schema({
   username: String,
   name: String,
   age: Number
 });
+// This defines the structure of a user document in MongoDB. 
+// It specifies that each user will have a username, name, and age, all with different data types.
 
-const User = mongoose.model("User", userSchema); // Define the model and store it in a variable
+// Create a model for the user
+const User = mongoose.model("User", userSchema);
+// This line creates a model named "User" based on the userSchema.
+// Models allow you to interact with MongoDB collections in a structured way.
+// The "User" model can be used to perform CRUD operations on the "User" collection.
 
-module.exports = User; // Export the User model
+module.exports = User;
+// Export the "User" model so that it can be accessed and used in other parts of your application.
