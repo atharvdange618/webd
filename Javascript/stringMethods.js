5f5f115571ae25a82c6230fb902683342bf4ce3f
atharvdange618 <atharvdange618@gmail.com>
2023-10-29 14:36:29 +0530

    learnt string methods upto slice

---
+++ Javascript/stringMethods.js
@@ -0,0 +1,18 @@
+//trim method
+let str = "Hello";
+console.log(str.trim());
+
+//toUpperCase method and toLowerCase method
+console.log(str.toUpperCase());
+console.log(str.toLowerCase());
+
+//indexOf method
+console.log(str.indexOf("o"));
+
+//method chaining
+console.log(str.toUpperCase().indexOf("L"));
+
+//slice method
+console.log(str.slice(1, 4));
+console.log(str.slice(-3));
+console.log(str.slice(1));
