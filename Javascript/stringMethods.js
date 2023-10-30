5dec7323c71114e9758fc57c1f2d075d4cd1adee
atharvdange618 <atharvdange618@gmail.com>
2023-10-30 23:58:29 +0530

    practice question completed

--- Javascript/stringMethods.js
+++ Javascript/stringMethods.js
@@ -16,3 +16,20 @@ console.log(str.toUpperCase().indexOf("L"));
 console.log(str.slice(1, 4));
 console.log(str.slice(-3));
 console.log(str.slice(1));
+
+//repeat
+console.log(str.repeat(3));
+
+//replace
+console.log(str.replace("l", "a"));
+
+//practice questions
+let msg = "help!";
+console.log(msg.trim.toUpperCase);
+
+let name = "atharvdange";
+console.log(name.slice(4, 9));
+console.log(name.indexOf("ha"));
+console.log(name.replace("atharv", "maithili"));
+
+console.log(name.slice(6, 11).replace("a", "t"));
