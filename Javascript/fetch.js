c6470a27989d51a8222e7a337465a479db0a6c75
atharvdange618 <atharvdange618@gmail.com>
2023-12-18 00:04:46 +0530

    learnt new fetch methods

--- Javascript/fetch.js
+++ Javascript/fetch.js
@@ -7,3 +7,8 @@ randomUser
     .catch(function (error) {
         console.log('Error:', error);
     });
+
+fetch("https://restcountries.com/v3.1/all").then(response => response.json())
+    .then(data => {
+        console.log(data);
+    })
