ee104f8209a520abfdd16498273697da9c85b8d2
atharvdange618 <atharvdange618@gmail.com>
2024-01-06 12:07:07 +0530

    added image upload and background functionality
    added the images to the background of the respective blogs

--- LearningNode/Blog Project/models/BlogPost.js
+++ LearningNode/Blog Project/models/BlogPost.js
@@ -5,10 +5,11 @@ const BlogPostSchema = new Schema({
     title: String,
     body: String,
     username: String,
-    datePosted: { /* can declare property type with an object like this because we need 'default' */
+    datePosted: {
         type: Date,
         default: new Date()
-    }
+    },
+    image: String
 });
 
 const BlogPost = mongoose.model('BlogPost', BlogPostSchema);
--- LearningNode/Blog Project/script.js
+++ LearningNode/Blog Project/script.js
@@ -5,6 +5,7 @@ const ejs = require('ejs');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const BlogPost = require('./models/BlogPost.js')
+const fileUpload = require('express-fileupload')
 
 mongoose.connect('mongodb://localhost:27017/blog_project');
 
@@ -13,6 +14,8 @@ app.use(express.static('public'));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(fileUpload());
 
 app.get('/', async (req, res) => {
     const blogposts = await BlogPost.find({})
@@ -30,25 +33,36 @@ app.get('/contact', (req, res) => {
 })
 
 app.get('/post/:id', async (req, res) => {
-    const blogpost = await BlogPost.findById(req.params.id)
-    res.render('post', {
-        blogpost
-    })
+    try {
+        const blogpost = await BlogPost.findById(req.params.id);
+        res.render('post', { blogpost });
+    } catch (error) {
+        console.log(error);
+        res.status(404).send('Post not found');
+    }
 })
+
 
 app.get('/posts/new', (req, res) => {
     res.render('create');
 })
 
 app.post('/posts/store', async (req, res) => {
-    await BlogPost.create(req.body)
-        .then(blogpost => {
-            res.redirect('/');
-        })
-        .catch(error => {
-            console.log(error);
+    try {
+        let image = req.files.image;
+        await image.mv(path.resolve(__dirname, 'public/assets/img', image.name));
+        await BlogPost.create({
+            ...req.body,
+            image: '/assets/img/' + image.name
         });
-})
+        res.redirect('/');
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Error uploading the file.');
+    }
+});
+
+
 
 app.listen(3000, () => {
     console.log("Server started on http://localhost:3000");
