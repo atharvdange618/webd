26dc6f793e418fa48a70b94fffddc258934c17d1
atharvdange618 <atharvdange618@gmail.com>
2024-01-23 22:57:15 +0530

    solved intermediate mongodb questions

--- LearningNode/backend/backend2/routes/index.js
+++ LearningNode/backend/backend2/routes/index.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+const userModel = require('./users');
+
 router.get('/', function (req, res) {
   res.render('index');
 });
@@ -15,6 +16,36 @@ router.get('/failed', function (req, res) {
 router.get('/checkkaro', function (req, res) {
   console.log(req.flash('age'), req.flash('name'));
   res.send("check karlo be ke terminal pe");
+});
+
+router.get('/create', async function (req, res) {
+  let userData = await userModel.create({
+    username: "Haru2003",
+    nickname: "kpopfan",
+    description: "A avid dancer and actor, cheerful soul",
+    categories: ['dance', 'acting', 'kpop music']
+  });
+  res.send(userData);
+});
+
+//to find all set of values
+router.get('/find', async function (req, res) {
+  var regex = new RegExp("atharv", 'i')
+  let user = await userModel.find({ username: regex });
+  res.send(user);
+});
+
+//to find docs with a specific date range
+router.get('/finddate', async function (req, res) {
+  var date1 = new Date("2024-01-23");
+  var date2 = new Date("2024-01-24");
+  let user = await userModel.find({ dateCreated: { $gte: date1, $lte: date2 } });
+  res.send(user);
+})
+
+//find docs based on the existence of the field
+router.get('/findfield', function (req, res) {
+  
 });
 
 module.exports = router;
