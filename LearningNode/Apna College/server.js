b2ca6110600470b6c80560c2b567a9261861d1fb
atharvdange618 <atharvdange618@gmail.com>
2024-03-08 20:50:50 +0530

    learnt: react basics

---
+++ LearningNode/Apna College/server.js
@@ -0,0 +1,8 @@
+const http = require('http')
+
+const server = http.createServer((req, res) => {
+    console.log(req.url)
+    res.end("Hello world")
+})
+
+server.listen(3000)
---
+++ React/deadpoolreacts/src/products.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function products() {
+    return (
+        <div className='text-white'>
+            <h1>hello nigga</h1>
+        </div>
+    )
+}
+
+export default products
