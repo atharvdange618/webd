4de9e28b5fc34c10e53840f0d9cc6c937ad3b6bd
Atharv Dange <atharvdange618@gmail.com>
2024-06-14 10:03:01 +0530

    fix: error handling and minor bug fixes

--- DNS_Server/index.js
+++ DNS_Server/index.js
@@ -1,10 +1,8 @@
-//require dgram package for creating udp server
-const dgram = require('node:dgram')
-const dnsPacket = require('dns-packet')
-const { type } = require('node:os')
+const dgram = require('node:dgram');
+const dnsPacket = require('dns-packet');
 
-//create server using udp types
-const server = dgram.createSocket('udp4')
+// Create server using udp types
+const server = dgram.createSocket('udp4');
 
 const db = {
     'atharvdange.dev': {
@@ -15,34 +13,42 @@ const db = {
         type: 'CNAME',
         data: 'hashnode.network'
     }
-
-}
+};
 
 server.on('message', (msg, rinfo) => {
-    const incomingReq = dnsPacket.decode(msg)
-    const ipFromDb = db[incomingReq.questions[0].name]
+    const incomingReq = dnsPacket.decode(msg);
+    const ipFromDb = db[incomingReq.questions[0].name];
 
-    const ans = dnsPacket.encode({
-        type: 'response',
-        id: incomingReq.id,
-        flags: dnsPacket.AUTHORITATIVE_ANSWER,
-        questions: incomingReq.questions,
-        answers: [{
-            type: ipFromDb.type,
-            class: 'IN',
-            name: incomingReq.questions[0].name,
-            db: ipFromDb.data
-        }]
-    })
+    if (ipFromDb) {
+        const ans = dnsPacket.encode({
+            type: 'response',
+            id: incomingReq.id,
+            flags: dnsPacket.AUTHORITATIVE_ANSWER,
+            questions: incomingReq.questions,
+            answers: [{
+                type: ipFromDb.type,
+                class: 'IN',
+                name: incomingReq.questions[0].name,
+                data: ipFromDb.data
+            }]
+        });
 
-    server.send(ans, rinfo.port, rinfo.address)
+        server.send(ans, rinfo.port, rinfo.address);
 
-    console.log({
-        msg: incomingReq.questions,
-        rinfo
-    })
-})
+        console.log({
+            msg: incomingReq.questions,
+            rinfo
+        });
+    } else {
+        console.log('Domain not found in DB:', incomingReq.questions[0].name);
+    }
+});
+
+server.on('error', (err) => {
+    console.error('Server error:', err);
+    server.close();
+});
 
 server.bind(53, () => {
-    console.log('DNS server is running on port 53')
-})
+    console.log('DNS server is running on port 53');
+});
