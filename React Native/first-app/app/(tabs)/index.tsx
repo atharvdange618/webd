204cbd5ac69774a8bbd5e46704fea22e21731608
Atharv Dange <atharvdange618@gmail.com>
2024-11-29 23:02:49 +0530

    learnt tab, stack and drawer navigation, and how to add icons to the tabs

--- React Native/first-app/app/(tabs)/index.tsx
+++ React Native/first-app/app/(tabs)/index.tsx
@@ -3,8 +3,8 @@ import React from 'react'
 
 const index = () => {
     return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text>Hello world</Text>
+        <View>
+            <Text>index</Text>
         </View>
     )
 }
