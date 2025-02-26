1050c6ad8da519173fe28df478dfcecc5eea3c13
Atharv Dange <atharv.d@smartscripts.in>
2025-02-26 15:43:54 +0530

    Major changes
    1) initialized markdown-notes-app in electron and completed it's basic setup
    2) learnt why await is used twice in fetch api calls
    3) added nodejs openai api integration project
    4) removed react ssr project
    5) completed country explorer project in react router v7

--- LearningNode/Helmet/index.js
+++ LearningNode/Helmet/index.js
@@ -3,12 +3,12 @@ import helmet from "helmet";
 
 const app = express();
 
-app.use(helmet())
+app.use(helmet());
 
 app.get("/", (req, res) => {
-    res.send("Hello World");
+  res.send("Hello World");
 });
 
 app.listen(3000, () => {
-    console.log("Server is running on port 3000");
+  console.log("Server is running on port 3000");
 });
